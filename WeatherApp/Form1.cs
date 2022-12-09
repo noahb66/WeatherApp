dedd254f1136eb97959ad486b2f89cf0370e19a3
noahb66 <noahb66@gmail.com>
2022-12-08 18:00:33 -0600

    added comments

--- WeatherApp/Form1.cs
+++ WeatherApp/Form1.cs
@@ -35,6 +35,7 @@ namespace WeatherApp
             {
                 string url = string.Format("https://api.openweathermap.org/data/2.5/weather?q={0}&appid={1}", city, APIKey);
                 var json = web.DownloadString(url);
+                //using the Newtonsoft Json convert class to process the json data
                 WeatherInfo.root Info = JsonConvert.DeserializeObject<WeatherInfo.root>(json);
 
                 picIcon.ImageLocation = "https://api.openweathermap.org/img/w/" + Info.weather[0].icon + ".png";
@@ -48,6 +49,7 @@ namespace WeatherApp
             }
         }
 
+        // converts the sunset and sunrise times to be in appropriate format
         DateTime convertDateTime(long time)
         {
             DateTime day = new DateTime(1970, 1, 1, 0, 0, 0, System.DateTimeKind.Utc).ToLocalTime();
